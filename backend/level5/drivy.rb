87dd45c6f48ecad7ec1de07e5bd2d0b35dda22ad
Miguel Salas <miguelsalas.git@gmail.com>
2019-08-12 01:39:02 +0200

    copy:  level 5.  copy files from the precedent level.

---
+++ backend/level5/drivy.rb
@@ -0,0 +1,97 @@
+require 'date'
+
+class Drivy
+  class << self
+    attr_accessor :cars, :rentals
+
+    def process(input)
+      check(input)
+      self.cars = input['cars'].inject({}) do |memo, car|
+        memo[car['id']] = car
+        memo
+      end
+      self.rentals = input['rentals']
+      calculate_each_rental
+    end
+
+    def check(input)
+      unless input['cars'].is_a?(Array)
+        raise ArgumentError, "\"input['cars']\" was not an Array. input['cars'] => #{input['cars'].inspect}."
+      end
+    end
+
+    def check_car(car, rental)
+      if car.nil?
+        raise ArgumentError, "rental['car_id'] => #{rental['car_id'].inspect} resulted in nil while looking up in Drivy.cars. " +
+          "Seems like that id not present in 'input['cars']'"
+      end
+    end
+
+    def calculate_each_rental
+      {
+        rentals: rentals.map do |rental|
+          rental_days = rental_days(rental['start_date'], rental['end_date'])
+          car = cars[rental['car_id']]
+          check_car(car, rental)
+          price = calculate_price(rental, car, rental_days)
+          commission = (price * 0.3).round
+          insurance, assistance, drivy = divide_commission(commission, rental_days)
+          owner_gains = (price - commission)
+
+          {
+            id: rental['id'],
+            actions: [
+              action('driver','debit', price),
+              action('owner','credit', owner_gains),
+              action('insurance','credit', insurance),
+              action('assistance','credit', assistance),
+              action('drivy','credit', drivy),
+            ]
+          }
+        end
+      }
+    end
+
+    def action(actor, type, amount)
+      {
+        who: actor,
+        type: type,
+        amount: amount
+      }
+    end
+
+    def calculate_price(rental, car, rental_days)
+      price_per_day = car['price_per_day']
+      time_price = time_price(rental_days, price_per_day)
+      distance_price = rental['distance'] * car['price_per_km']
+      time_price + distance_price
+    end
+
+    def rental_days(start_date, end_date)
+      (Date.parse(start_date)..Date.parse(end_date)).count
+    end
+
+    def time_price(days, price_per_day)
+      (1..days).inject(0) do |sum, day_number|
+        case day_number
+        when 1
+          sum += price_per_day
+        when 2..4
+          sum += price_per_day * 0.9
+        when 5..10
+          sum += price_per_day * 0.7
+        when 11..Float::INFINITY
+          sum += price_per_day * 0.5
+        end
+      end.round
+    end
+
+    def divide_commission(commission, rental_days)
+      insurance = commission / 2
+      assistance = 100 * rental_days
+      drivy = commission - (insurance + assistance)
+      [insurance, assistance, drivy]
+    end
+
+  end
+end
